23a95efe57abd28d2216e2e11aca7d850d4577f0
Javier <javier@MacBook-Pro-de-Javier.local>
2019-03-24 02:06:45 -0400

    Configure database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("HELLO!");
+import express from "express";
+import mongoose from "mongoose";
+import keys from "../config/keys";
+
+const app = express();
+
+//Connect to database
+mongoose
+  .connect(keys.MongoURI)
+  .then(() => {
+    console.log("Mongo DB Connected");
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// uste the environment port if its available otherwise Use 500 in development
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
